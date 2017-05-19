4b8a757e2b875b7e2d6950b17f936a2683e94cf5
Mark Raynsford <code@io7m.com>
2017-05-19 17:37:59 +0000

    Correct polygon generation bug

--- src/main/java/com/io7m/roommodel0/RoomModelLiquidCells.java
+++ src/main/java/com/io7m/roommodel0/RoomModelLiquidCells.java
@@ -6,11 +6,13 @@ import com.io7m.jregions.core.unparameterized.areas.AreasI;
 import com.io7m.jspatial.api.quadtrees.QuadTreeReadableIType;
 import com.io7m.jtensors.core.unparameterized.vectors.Vector2D;
 import com.io7m.jtensors.core.unparameterized.vectors.Vector2I;
+import com.io7m.junreachable.UnreachableCodeException;
 import com.io7m.roommodel0.mesh.Mesh;
 import com.io7m.roommodel0.mesh.MeshReadableType;
 import com.io7m.roommodel0.mesh.MeshType;
 import com.io7m.roommodel0.mesh.PolygonEdgeType;
 import com.io7m.roommodel0.mesh.PolygonType;
+import com.io7m.roommodel0.mesh.PolygonVertexID;
 import com.io7m.roommodel0.mesh.PolygonVertexType;
 import it.unimi.dsi.fastutil.ints.IntRBTreeSet;
 import it.unimi.dsi.fastutil.objects.ReferenceArrayList;
@@ -19,6 +21,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static com.io7m.jnull.NullCheck.notNull;
 
@@ -49,73 +53,7 @@ public final class RoomModelLiquidCells
     final IntRBTreeSet y_values = collectVertices(mesh);
     int y_previous = bounds.minimumY();
     for (final int y_current : y_values) {
-
-      final AreaI area =
-        AreaI.of(bounds.minimumX(), bounds.maximumX(), y_previous, y_current);
-
-      final ReferenceArrayList<PolygonEdgeType> edges_sorted =
-        collectEdges(tree, area);
-      final ReferenceArrayList<EdgeRecord> records =
-        collectEdgeRecords(edges_sorted, area);
-
-      LOG.debug(
-        "{}:{} edges {} ({})",
-        Integer.valueOf(y_previous),
-        Integer.valueOf(y_current),
-        Integer.valueOf(edges_sorted.size()),
-        Integer.valueOf(records.size()));
-
-      boolean inside_polygon = false;
-      for (int index = 1; index < records.size(); ++index) {
-        final EdgeRecord record0 = records.get(index - 1);
-        final EdgeRecord record1 = records.get(index);
-
-        inside_polygon = !inside_polygon;
-        if (inside_polygon) {
-
-          final Vector2I v0p = record0.intersection_max;
-          final Vector2I v1p = record0.intersection_min;
-          final Vector2I v2p = record1.intersection_min;
-          final Vector2I v3p = record1.intersection_max;
-
-          {
-            final ReferenceArrayList<Vector2I> vs = new ReferenceArrayList<>(4);
-            vs.add(v0p);
-            vs.add(v1p);
-            vs.add(v2p);
-            vs.add(v3p);
-
-            if (vs.stream().distinct().count() != 4L) {
-              LOG.debug("discarding zero size poly");
-              continue;
-            }
-          }
-
-          final PolygonVertexType v0 =
-            cell_mesh.vertexFind(v0p).orElse(cell_mesh.vertexCreate(v0p));
-          final PolygonVertexType v1 =
-            cell_mesh.vertexFind(v1p).orElse(cell_mesh.vertexCreate(v1p));
-          final PolygonVertexType v2 =
-            cell_mesh.vertexFind(v2p).orElse(cell_mesh.vertexCreate(v2p));
-          final PolygonVertexType v3 =
-            cell_mesh.vertexFind(v3p).orElse(cell_mesh.vertexCreate(v3p));
-
-          LOG.debug("create {} {} {} {}", v0, v1, v2, v3);
-
-          final PolygonType polygon =
-            cell_mesh.polygonCreateVV(v0, v1, v2, v3);
-
-          for (final PolygonVertexType v : polygon.vertices()) {
-            for (final PolygonType vp : v.polygons()) {
-              LOG.debug(
-                "{} touches {}",
-                Long.valueOf(polygon.id().value()),
-                Long.valueOf(vp.id().value()));
-            }
-          }
-        }
-      }
-
+      processSpan(cell_mesh, tree, bounds, y_previous, y_current);
       y_previous = y_current;
     }
 
@@ -123,6 +61,92 @@ public final class RoomModelLiquidCells
       "created {} polygons",
       Integer.valueOf(cell_mesh.polygons().size()));
     return new RoomModelLiquidCells(cell_mesh);
+  }
+
+  private static void processSpan(
+    final MeshType cell_mesh,
+    final QuadTreeReadableIType<PolygonType> tree,
+    final AreaI span_bounds,
+    final int y_previous,
+    final int y_current)
+  {
+    final AreaI area =
+      AreaI.of(
+        span_bounds.minimumX(),
+        span_bounds.maximumX(),
+        y_previous,
+        y_current);
+
+    final ReferenceArrayList<PolygonEdgeType> edges_sorted =
+      collectEdges(tree, area);
+    final ReferenceArrayList<EdgeRecord> records =
+      collectEdgeRecords(edges_sorted, area);
+
+    LOG.debug(
+      "{}:{} edges {} ({})",
+      Integer.valueOf(y_previous),
+      Integer.valueOf(y_current),
+      Integer.valueOf(edges_sorted.size()),
+      Integer.valueOf(records.size()));
+
+    boolean inside_polygon = false;
+    for (int index = 1; index < records.size(); ++index) {
+      final EdgeRecord record0 = records.get(index - 1);
+      final EdgeRecord record1 = records.get(index);
+
+      inside_polygon = !inside_polygon;
+      if (!inside_polygon) {
+        continue;
+      }
+
+      final ReferenceArrayList<Vector2I> positions =
+        new ReferenceArrayList<>(4);
+      positions.add(record0.intersection_max);
+      positions.add(record0.intersection_min);
+      positions.add(record1.intersection_min);
+      positions.add(record1.intersection_max);
+
+      final List<Vector2I> distinct_positions =
+        positions.stream().distinct().collect(Collectors.toList());
+
+      final PolygonType polygon;
+      switch (distinct_positions.size()) {
+        case 3: {
+          final Vector2I v0p = distinct_positions.get(0);
+          final Vector2I v1p = distinct_positions.get(1);
+          final Vector2I v2p = distinct_positions.get(2);
+          polygon = cell_mesh.polygonCreateVV(
+            cell_mesh.vertexFind(v0p).orElse(cell_mesh.vertexCreate(v0p)),
+            cell_mesh.vertexFind(v1p).orElse(cell_mesh.vertexCreate(v1p)),
+            cell_mesh.vertexFind(v2p).orElse(cell_mesh.vertexCreate(v2p)));
+          break;
+        }
+        case 4: {
+          final Vector2I v0p = distinct_positions.get(0);
+          final Vector2I v1p = distinct_positions.get(1);
+          final Vector2I v2p = distinct_positions.get(2);
+          final Vector2I v3p = distinct_positions.get(3);
+          polygon = cell_mesh.polygonCreateVV(
+            cell_mesh.vertexFind(v0p).orElse(cell_mesh.vertexCreate(v0p)),
+            cell_mesh.vertexFind(v1p).orElse(cell_mesh.vertexCreate(v1p)),
+            cell_mesh.vertexFind(v2p).orElse(cell_mesh.vertexCreate(v2p)),
+            cell_mesh.vertexFind(v3p).orElse(cell_mesh.vertexCreate(v3p)));
+          break;
+        }
+        default: {
+          throw new UnreachableCodeException();
+        }
+      }
+
+      for (final PolygonVertexType v : polygon.vertices()) {
+        for (final PolygonType vp : v.polygons()) {
+          LOG.debug(
+            "{} touches {}",
+            Long.valueOf(polygon.id().value()),
+            Long.valueOf(vp.id().value()));
+        }
+      }
+    }
   }
 
   public MeshReadableType mesh()
